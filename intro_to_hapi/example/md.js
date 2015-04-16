616de5df0e2a905753c3246510c7c44ac7efc072
Thomas Wilburn <tbone@thomaswilburn.net>
2015-04-16 12:11:02 -0700

    Add comments to MarkDown handler

--- intro_to_hapi/example/md.js
+++ intro_to_hapi/example/md.js
@@ -5,17 +5,24 @@ var path = require("path");
 var renderer = new markdown.HtmlRenderer();
 var parser = new markdown.Parser();
 
+// This is our request handler
 module.exports = function(request, reply) {
+  //get the current filename, or default to "index.md"
   var filename = request.params.file || "index";
   var filePath = path.join(__dirname, filename + ".md");
+  //does this file exist?
   fs.stat(filePath, function(err) {
+    //file not found, 404
     if (err) return reply("File not found").code(404);
+    //load the file each time
     fs.readFile(filePath, "utf8", function(err, file) {
-      // file not found
       if (err) return reply("File couldn't be read").code(500);
-      var parsed = parser.parse(file);
-      var html = renderer.render(parsed);
-      if (!parsed || !html) {
+      var html;
+      try {
+        //parse text into HTML using CommonMark
+        var parsed = parser.parse(file);
+        html = renderer.render(parsed);
+      } catch (e) {
         return reply("Couldn't parse MarkDown").code(500);
       }
       reply(html);
